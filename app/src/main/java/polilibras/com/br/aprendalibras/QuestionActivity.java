c3bdfa294f1e80e53d45580d062c11fd6d6cba50
marcelo.koga <marcelo.koga@polilibras.com.br>
2016-02-15 19:51:30 -0200

    Tudo na mesma activity

--- app/src/main/java/polilibras/com/br/aprendalibras/QuestionActivity.java
+++ app/src/main/java/polilibras/com/br/aprendalibras/QuestionActivity.java
@@ -1,6 +1,5 @@
 package polilibras.com.br.aprendalibras;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
@@ -9,49 +8,134 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class QuestionActivity extends VideoActivity {
+public class QuestionActivity extends AppCompatActivity {
+
+    protected QuestionActivity activity = this;
+
+    private VideoView mVideoView;
+    private View mOptionsPanel;
+    private View mAnswerPanel;
+
+    private Question mCurrentQuestion;
+
+    // Options
+    private List<Button> mOptionButtons;
+
+    // Answer
+    private TextView mAnswerMsg;
+    private TextView mCorrectAnswerTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_activity);
 
-        Question question = QuestionProvider.getInstance().getNextQuestion();
-        mVideoView.setVideoURI(Uri.parse(question.getSource()));
+        mCurrentQuestion = QuestionProvider.getInstance().getNextQuestion();
 
-        List<Button> optionBtns = new ArrayList<>(4);
-        optionBtns.add((Button) findViewById(R.id.question_option1_btn));
-        optionBtns.add((Button) findViewById(R.id.question_option2_btn));
-        optionBtns.add((Button) findViewById(R.id.question_option3_btn));
-        optionBtns.add((Button) findViewById(R.id.question_option4_btn));
+        initQuestion();
+        initOptions();
+        initAnswer();
 
-        for (int i = 0; i < optionBtns.size(); i++) {
-            optionBtns.get(i).setText(question.getOptions().get(i).toUpperCase());
-            optionBtns.get(i).setOnClickListener(new OptionClickListener(activity, question.getCorrectAnswer() == i));
+        showQuestion();
+        showOptions();
+    }
+
+    private void initQuestion() {
+
+        mVideoView = (VideoView) findViewById(R.id.video_view);
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mVideoView.start();
+            }
+        });
+        findViewById(R.id.video_repeat_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mVideoView.seekTo(0);
+                mVideoView.start();
+            }
+        });
+    }
+
+    private void showQuestion() {
+        mVideoView.setVideoURI(Uri.parse(mCurrentQuestion.getSource()));
+    }
+
+    private void initOptions() {
+        mOptionsPanel = findViewById(R.id.options_layout);
+        mOptionButtons = new ArrayList<>(4);
+        mOptionButtons.add((Button) findViewById(R.id.question_option1_btn));
+        mOptionButtons.add((Button) findViewById(R.id.question_option2_btn));
+        mOptionButtons.add((Button) findViewById(R.id.question_option3_btn));
+        mOptionButtons.add((Button) findViewById(R.id.question_option4_btn));
+        for (int i = 0; i < mOptionButtons.size(); i++) {
+            mOptionButtons.get(i).setOnClickListener(new OptionClickListener(i));
         }
+    }
+
+    private void showOptions() {
+        mOptionsPanel.setVisibility(View.VISIBLE);
+        mAnswerPanel.setVisibility(View.GONE);
+
+        for (int i = 0; i < mOptionButtons.size(); i++) {
+            mOptionButtons.get(i).setText(mCurrentQuestion.getOptions().get(i).toUpperCase());
+        }
+    }
+
+    private void initAnswer() {
+        mAnswerPanel = findViewById(R.id.answer_layout);
+        mAnswerMsg = (TextView) findViewById(R.id.answer_msg_txt);
+        mCorrectAnswerTxt = (TextView) findViewById(R.id.answer_correct_txt);
+        findViewById(R.id.answer_next_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentQuestion = QuestionProvider.getInstance().getNextQuestion();
+                showQuestion();
+                showOptions();
+            }
+        });
+    }
+
+    private void showAnswer(int option) {
+
+        mOptionsPanel.setVisibility(View.GONE);
+        mAnswerPanel.setVisibility(View.VISIBLE);
+
+        boolean answerIsCorrect = (mCurrentQuestion.getCorrectAnswer() == option);
+
+        mAnswerMsg.setText(answerIsCorrect ? "PARABÉNS!!!" : "NÃO DEU...");
+        mCorrectAnswerTxt.setText(getResources().getString(R.string.correct_answer,
+                mCurrentQuestion.getOptions().get(mCurrentQuestion.getCorrectAnswer())));
+
+
     }
 
     private class OptionClickListener implements View.OnClickListener {
 
-        private boolean mCorrectOption;
-        private Context mContext;
+        private int mOption;
 
-        public OptionClickListener(Context context, boolean correctOption) {
-            mContext = context;
-            mCorrectOption = correctOption;
+        public OptionClickListener(int option) {
+            super();
+            mOption = option;
         }
 
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(mContext, AnswerActivity.class);
-            intent.putExtra(AnswerActivity.INTENT_CORRECT_ANSWER_BOOL, mCorrectOption);
-            startActivity(intent);
-            finish();
+            showAnswer(mOption);
         }
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mVideoView.stopPlayback();
     }
 }
