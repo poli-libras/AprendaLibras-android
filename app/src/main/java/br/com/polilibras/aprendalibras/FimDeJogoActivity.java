68fea3210786a423d2304d05a2f43955958dee23
Guilherme Yoshita <guiyoshita@gmail.com>
2016-04-01 17:53:12 -0300

    Guardar recorde

--- app/src/main/java/br/com/polilibras/aprendalibras/FimDeJogoActivity.java
+++ app/src/main/java/br/com/polilibras/aprendalibras/FimDeJogoActivity.java
@@ -1,6 +1,8 @@
 package br.com.polilibras.aprendalibras;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,7 @@ public class FimDeJogoActivity extends AppCompatActivity {
 
     public final static String GANHOU_EXTRA = "GANHOU";
     public final static String EXTRA_ACERTOS = "ACERTOS";
+    int newHighScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +25,27 @@ public class FimDeJogoActivity extends AppCompatActivity {
         TextView text = (TextView) findViewById(R.id.fim_jogo_mensagem_txt);
         if (ganhar == true) {
             text.setText(getString(R.string.fim_jogo_ganhador_txt));
-        }
-        else{
+        } else {
             text.setText(getString(R.string.fim_jogo_perdedor));
         }
         TextView textView = (TextView) findViewById(R.id.fim_jogo_pontuacao_txt);
-        textView.setText(getString(R.string.pontuacao_txt, pontuacao));}
+        textView.setText(getString(R.string.pontuacao_txt, pontuacao));
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        newHighScore = sharedPref.getInt("pontuacao", 0);
+
+        if (newHighScore > pontuacao) {
+            TextView textView2 = (TextView) findViewById(R.id.recorde_txt);
+            textView2.setText(getString(R.string.high_score_txt, newHighScore));
+        } else {
+            newHighScore = pontuacao;
+            TextView textView2 = (TextView) findViewById(R.id.recorde_txt);
+            textView2.setText(getString(R.string.high_score_txt, newHighScore));
+            sharedPref.edit().putInt("pontuacao", newHighScore).apply();
+        }
+
+    }
+
 
     public void sendMessage1(View view){
         Intent intent = new Intent(this,QuestionActivity.class);
